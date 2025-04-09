09994a62875e8acc63d076dd3515ea77a7728183
Joseph Avila <avilajoseph2021@gmail.com>
2025-04-09 17:03:40 -0500

    Por corregir

--- src/app/Doc/Layout/DocumentView/DocumentView.component.ts
+++ src/app/Doc/Layout/DocumentView/DocumentView.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MenuComponent } from "../../Component/Menu/Menu.component";
+import { ContainerViewComponent } from "../../Component/ContainerView/ContainerView.component";
 
 @Component({
   selector: 'app-document-view',
-  imports: [MenuComponent],
+  imports: [MenuComponent, ContainerViewComponent],
   templateUrl: './DocumentView.component.html',
   styleUrl: './DocumentView.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
