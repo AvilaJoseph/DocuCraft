efcc0dd256bbf7d3fd4df13a5d652f362ca937dd
AvilaJoseph <avilajoseph2021@gmail.com>
2025-04-09 22:11:16 -0500

    Re-estructuración correcta

--- src/app/Doc/Layout/DocumentView/DocumentView.component.ts
+++ src/app/Doc/Layout/DocumentView/DocumentView.component.ts
@@ -1,10 +1,15 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MenuComponent } from "../../Component/Menu/Menu.component";
 import { ContainerViewComponent } from "../../Component/ContainerView/ContainerView.component";
+import { FormSideComponent } from '../../Component/FormSide/FormSide.component';
 
 @Component({
   selector: 'app-document-view',
-  imports: [MenuComponent, ContainerViewComponent],
+  imports: [
+    MenuComponent, 
+    ContainerViewComponent,
+    FormSideComponent
+  ],
   templateUrl: './DocumentView.component.html',
   styleUrl: './DocumentView.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
