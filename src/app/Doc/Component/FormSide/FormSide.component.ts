63aa8b4fefb6c3ab0bdf72e781c3a1797fd8e463
AvilaJoseph <avilajoseph2021@gmail.com>
2025-04-10 22:45:22 -0500

    Funcionalidad de aparecer forms extra

--- src/app/Doc/Component/FormSide/FormSide.component.ts
+++ src/app/Doc/Component/FormSide/FormSide.component.ts
@@ -1,10 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-form-side',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './FormSide.component.html',
   styleUrl: './FormSide.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FormSideComponent { }
+export class FormSideComponent { 
+
+  showAdditionalFields = false;
+
+  toggleAdditionalFields() {
+    this.showAdditionalFields = !this.showAdditionalFields;
+  }
+}
--- src/app/Doc/Layout/DocumentView/DocumentView.component.ts
+++ src/app/Doc/Layout/DocumentView/DocumentView.component.ts
@@ -2,13 +2,15 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MenuComponent } from "../../Component/Menu/Menu.component";
 import { ContainerViewComponent } from "../../Component/ContainerView/ContainerView.component";
 import { FormSideComponent } from '../../Component/FormSide/FormSide.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-document-view',
   imports: [
     MenuComponent, 
     ContainerViewComponent,
-    FormSideComponent
+    FormSideComponent,
+    CommonModule
   ],
   templateUrl: './DocumentView.component.html',
   styleUrl: './DocumentView.component.css',
