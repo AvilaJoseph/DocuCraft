f5d8ea456f91e7d01954a68bec9943eb16db1ef8
Joseph Avila <avilajoseph2021@gmail.com>
2025-04-15 11:13:15 -0500

    Implementacion de form back

--- src/app/Doc/Component/FormSide/FormSide.component.ts
+++ src/app/Doc/Component/FormSide/FormSide.component.ts
@@ -1,11 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { BasicFormComponent } from "./sub-component/BasicForm/BasicForm.component";
+import { SectionFormComponent } from "./sub-component/SectionForm/SectionForm.component";
 
 @Component({
   selector: 'app-form-side',
   imports: [
-    CommonModule
-  ],
+    CommonModule,
+    BasicFormComponent,
+    SectionFormComponent
+],
   templateUrl: './FormSide.component.html',
   styleUrl: './FormSide.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
--- src/app/Doc/Component/FormSide/sub-component/SectionForm/SectionForm.component.ts
+++ src/app/Doc/Component/FormSide/sub-component/SectionForm/SectionForm.component.ts
@@ -1,12 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-section-form',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './SectionForm.component.html',
   styleUrl: './SectionForm.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SectionFormComponent {
   
+  icons_show = false;
+  showAdditionalFields = false;
+
+  toggleAdditionalFields() {
+    this.showAdditionalFields = !this.showAdditionalFields;
+    this.icons_show = !this.icons_show;
+  }
+
+
  }
